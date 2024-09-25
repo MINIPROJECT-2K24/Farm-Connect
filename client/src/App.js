893c7cc7794d61bd398e897ce6001d97952a0cd8
arun <arunsudhakar18@gmail.com>
2024-09-25 13:57:34 +0530

    tailwind integrated👍

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
-function App() {
-  return <></>;
-}
+import "./index.css";
 
-export default App;
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline text-red-500">Hello world!</h1>
+  );
+}
